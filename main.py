5b2a48ca31562fb78ad651dcd52a244855cbf446
holy0spirit <107628145+holy0spirit@users.noreply.github.com>
2022-06-24 07:27:00 -0700

    Update main.py
    ignoring heroku server

--- main.py
+++ main.py
@@ -9,3 +9,26 @@ def testAPI():
 
     return jsonify({"data" : "end-point successful", "status" : "200"})
 
+@app.route('/palindrome', methods=['POST'])
+def mPalindrome():
+    mString = request.json
+    mOriginalText = mString["original-text"]
+    mStringLen = len(mOriginalText)
+    mTextArray = []
+    for s in mOriginalText:
+        mStringLen = mStringLen - 1
+        mTextArray.append(mOriginalText[mStringLen])
+
+    mSecondaryText = ''.join(mTextArray)
+
+    if mOriginalText == mSecondaryText:
+        # print("True, it is a Palindrome Text")
+        return jsonify({"message" : "True, it is a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
+    else:
+        # print("False, it is not a Palindrome Text")
+        return jsonify({"message" : "False, it is not a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
+    
+    return '200'
+
+
+

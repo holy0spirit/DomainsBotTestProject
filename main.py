84f746156788a494396f490a45ad5ab8563532ef
holy0spirit <107628145+holy0spirit@users.noreply.github.com>
2022-06-24 08:00:51 -0700

    Update Main.py
    project completed

--- main.py
+++ main.py
@@ -3,32 +3,59 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def testAPI():
-
-
-    return jsonify({"data" : "end-point successful", "status" : "200"})
-
-@app.route('/palindrome', methods=['POST'])
-def mPalindrome():
-    mString = request.json
-    mOriginalText = mString["original-text"]
-    mStringLen = len(mOriginalText)
-    mTextArray = []
-    for s in mOriginalText:
-        mStringLen = mStringLen - 1
-        mTextArray.append(mOriginalText[mStringLen])
-
-    mSecondaryText = ''.join(mTextArray)
-
-    if mOriginalText == mSecondaryText:
-        # print("True, it is a Palindrome Text")
-        return jsonify({"message" : "True, it is a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
+    if request.method == 'GET':
+        try:
+            return jsonify({"data" : "end-point successful", "status" : "200"})
+        except:
+            return jsonify({"status" : "503", "message" : "App Crash"})
     else:
-        # print("False, it is not a Palindrome Text")
-        return jsonify({"message" : "False, it is not a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
-    
-    return '200'
+        return jsonify({"status" : "405", "message" : "Method Not Allowed"})
 
 
+@app.route('/palindrome', methods=['GET', 'POST'])
+def mPalindrome():
+    if request.method == 'POST':
+        try:
+            mString = request.json
+            mOriginalText = mString["text"]
+            mStringLen = len(mOriginalText)
+            mTextArray = []
+            for s in mOriginalText:
+                mStringLen = mStringLen - 1
+                mTextArray.append(mOriginalText[mStringLen])
 
+            mSecondaryText = ''.join(mTextArray)
+
+            if mOriginalText == mSecondaryText:
+                # print("True, it is a Palindrome Text")
+                return jsonify({"status" : "200", "message" : "True, it is a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
+            else:
+                # print("False, it is not a Palindrome Text")
+                return jsonify({"status" : "200", "message" : "False, it is not a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})   
+        except:
+            return jsonify({"status" : "503", "message" : "App Crash"})
+    else:
+        return jsonify({"status" : "405", "message" : "Method Not Allowed"})
+
+
+@app.route('/lenghty', methods=['GET', 'POST'])
+def mLenghty():
+    if request.method == 'POST':
+        try:
+            mS = request.json
+            mString = mS["text"]
+            mSelected = []
+
+            for s in mString:
+                if s not in mSelected:
+                    mSelected.append(s)
+            
+            mLenght = len(mSelected)
+
+            return jsonify({"status" : "200", "lenght" : mLenght, "sub-string" : mString, "character" : ''.join(mSelected)})
+        except:
+            return jsonify({"status" : "503", "message" : "App Crash"})
+    else:
+        return jsonify({"status" : "405", "message" : "Method Not Allowed"})

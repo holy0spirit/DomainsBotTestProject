6e0c7a1dded0777964c2a2823ab42581f6d081ad
holy0spirit <107628145+holy0spirit@users.noreply.github.com>
2022-06-24 06:51:32 -0700

    Update main.py
    Completed the Palindrome Task

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter.messagebox import RETRY
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -7,4 +8,25 @@ def testAPI():
 
 
     return jsonify({"data" : "end-point successful"})
+
+@app.route('/palindrome', methods=['POST'])
+def mPalindrome():
+    mString = request.json
+    mOriginalText = mString["original-text"]
+    mStringLen = len(mOriginalText)
+    mTextArray = []
+    for s in mOriginalText:
+        mStringLen = mStringLen - 1
+        mTextArray.append(mOriginalText[mStringLen])
+
+    mSecondaryText = ''.join(mTextArray)
+
+    if mOriginalText == mSecondaryText:
+        # print("True, it is a Palindrome Text")
+        return jsonify({"message" : "True, it is a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
+    else:
+        # print("False, it is not a Palindrome Text")
+        return jsonify({"message" : "False, it is not a Palindrome Text", "original-text" : mOriginalText, "palindrome-text" : mSecondaryText})
+    
+    return '200'
 
